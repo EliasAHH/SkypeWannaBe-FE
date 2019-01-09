321f2d07eb8cbced5ecd2096b47d9dc3ccd47257
Flatiron School <flatironschool@Flatirons-MacBook-Air-8.local>
2019-01-09 06:26:55 -0500

    created a video container to hold the canvas

---
+++ src/containers/Video.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const VideoContainer = () => {
+  return (
+    <div>
+      <video  id="local-video"  autoPlay> </video>
+    </div>
+  )
+}
+
+export default VideoContainer
